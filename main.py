18c2f350b739263555e05d3ede674cf8195dbe29
DanielVenette <danvenette@gmail.com>
2022-11-22 03:02:11 -0500

    11/22 03:00

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
+import tkinter
 from tkinter import *
 
 # create program window
 window = Tk()
 window.title("Watermark Generator")
+window.minsize(width=500, height=300)
 
+#label that instructs user how to use program
+instruction_label = Label(text="Click 'Browse' to find an image to add a watermark to",
+                          font=("Arial", 12, "bold")
+                          )
+instruction_label.pack(side="left")
 
 
 
